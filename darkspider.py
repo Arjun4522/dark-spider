4ed3da856e7da2ccf40fb7900e155befd73b34a2
Arjun Ghoshal <94633408+Arjun4522@users.noreply.github.com>
2023-10-14 20:34:26 +0530

    Update darkspider.py

--- darkspider.py
+++ darkspider.py
@@ -6,27 +6,23 @@ from stem import Signal
 from stem.control import Controller
 import socks
 
-# Function to change the IP address through Tor
 def change_ip_address():
     with Controller.from_port(port=9051) as controller:
-        controller.authenticate(password="16:D1A5FD4AAEFA57B9603244222A2D9481176AC38FA45511453E9E236DB5")
+        controller.authenticate(password="tor_config_password")
         controller.signal(Signal.NEWNYM)
 
-# Function to scrape .onion links using Tor
 def scrape_onion_links(url, max_depth=3, current_depth=1, visited_links=None, max_links_per_page=2):
     if visited_links is None:
         visited_links = set()
 
-    change_ip_address()  # Change to a new IP address through Tor
+    change_ip_address()  
 
-    # Configure the session with SOCKS proxy
     session = requests.session()
     session.proxies = {
-        "http": "socks5h://localhost:9050",  # Replace with your SOCKS proxy configuration
+        "http": "socks5h://localhost:9050", 
         "https": "socks5h://localhost:9050"
     }
 
-    # Make an HTTP request to the .onion URL through Tor
     try:
         print(f"Scraping: {url}")
         response = session.get(url)
@@ -35,16 +31,12 @@ def scrape_onion_links(url, max_depth=3, current_depth=1, visited_links=None, ma
         print(f"Request error: {e}")
         return
 
-    # Parse the HTML content
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Find and collect links on the page
     links = [a['href'] for a in soup.find_all('a', href=True)]
 
-    # Create a dictionary for the current .onion URL
     current_page_links = []
 
-    # Process and append the links, limiting to max_links_per_page
     link_count = 0
     for link in links:
         if link_count >= max_links_per_page:
@@ -55,26 +47,21 @@ def scrape_onion_links(url, max_depth=3, current_depth=1, visited_links=None, ma
             visited_links.add(absolute_link)
             current_page_links.append(absolute_link)
 
-            # Recursively scrape .onion links up to the specified depth
             if current_depth < max_depth:
                 scrape_onion_links(absolute_link, max_depth, current_depth + 1, visited_links, max_links_per_page=max_links_per_page)
 
     return current_page_links
 
-# Input parameters
 root_url = input("\nEnter the root .onion URL: ")
 max_depth = int(input("Enter the maximum depth: "))
 max_links_per_page = int(input("Enter the maximum links per page: "))
 
 print("\nScraping .onion links...\n")
 
-# Create a set to keep track of visited links
 visited_links = set()
 
-# Start scraping from the root .onion URL using Tor
 scraped_links = {root_url: scrape_onion_links(root_url, max_depth, 1, visited_links, max_links_per_page=max_links_per_page)}
 
-# Save the result to a JSON file
 with open('scraped_onion_links_tree.json', 'w') as json_file:
     json.dump(scraped_links, json_file, indent=4)
 
